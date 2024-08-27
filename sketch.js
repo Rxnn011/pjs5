93a918b769b574e7445356f2c4833aa68fd09f00
Rxnn011 <123667926+Rxnn011@users.noreply.github.com>
2024-08-27 11:38:07 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(600, 600);
+  background("black");
+}
+
+function draw() {
+  stroke("blue"):
+  fill("red");
+  
+  background(220);
+  rect(mouseX, mouseY, 20, 35);
+}
+  
+}
